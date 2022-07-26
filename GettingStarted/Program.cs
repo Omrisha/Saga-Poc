89c50a99215608f8d17329d5e6fd38149f799d10
Omri Shapira <omri.shapira@projectverte.com>
2022-07-26 17:30:07 +0300

    add docker compose

--- GettingStarted/Program.cs
+++ GettingStarted/Program.cs
@@ -1,6 +1,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MassTransit;
+using SagaPoc.Sagas;
+using Microsoft.EntityFrameworkCore;
+using SagaPoc.PersistanceModel;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace GettingStarted
 {
@@ -8,23 +13,50 @@ namespace GettingStarted
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var app = CreateHostBuilder(args).Build();
+            app.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(hostConfig =>
+                {
+                    hostConfig.SetBasePath(Directory.GetCurrentDirectory());
+                    hostConfig.AddJsonFile("appsettings.json", optional: true);
+
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
+                    var connString = hostContext.Configuration["ConnectionString"];
+                    services.AddDbContext<OrderSagaDbContext>((provider, builder) =>
+                    {
+                        builder.UseSqlServer(connString, m =>
+                        {
+                            m.MigrationsAssembly("SagaPoc.PersistanceModel");
+                            m.MigrationsHistoryTable($"__{nameof(OrderSagaDbContext)}");
+                        });
+                    });
                     services.AddMassTransit(x =>
                     {
-                        x.AddConsumer<MessageConsumer>();
+                        x.AddSaga<OrderSaga>()
+                        .EntityFrameworkRepository(r =>
+                        {
+                            r.ExistingDbContext<OrderSagaDbContext>();
+                            r.ConcurrencyMode = ConcurrencyMode.Pessimistic;
+                        });
 
-                        x.UsingRabbitMq((context,cfg) =>
+                        x.AddSaga<OrderPaymentSaga>()
+                        .EntityFrameworkRepository(r =>
+                        {
+                            r.ExistingDbContext<OrderSagaDbContext>();
+                            r.ConcurrencyMode = ConcurrencyMode.Pessimistic;
+                        });                        
+
+                        x.UsingRabbitMq((context, cfg) =>
                         {
                             cfg.ConfigureEndpoints(context);
                         });
                     });
-
                     services.AddHostedService<Worker>();
                 });
     }
