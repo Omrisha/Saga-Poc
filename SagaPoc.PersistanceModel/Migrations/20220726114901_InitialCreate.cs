bbd01575ae7c128974f70f2e6af394a85dffa856
Omri Shapira <omri.shapira@projectverte.com>
2022-07-26 17:29:30 +0300

    add persistance model

---
+++ SagaPoc.PersistanceModel/Migrations/20220726114901_InitialCreate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SagaPoc.PersistanceModel.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderPaymentSaga",
+                columns: table => new
+                {
+                    CorrelationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    InvoiceDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderPaymentSaga", x => x.CorrelationId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderSaga",
+                columns: table => new
+                {
+                    CorrelationId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubmitDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    AcceptDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ShipDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderSaga", x => x.CorrelationId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderPaymentSaga");
+
+            migrationBuilder.DropTable(
+                name: "OrderSaga");
+        }
+    }
+}
---
+++ SagaPoc.PersistanceModel/OrderSagaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit.EntityFrameworkCoreIntegration;
+using Microsoft.EntityFrameworkCore;
+using SagaPoc.Sagas.Mapping;
+
+namespace SagaPoc.PersistanceModel
+{
+    public class OrderSagaDbContext :
+        SagaDbContext
+    {
+        public OrderSagaDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override IEnumerable<ISagaClassMap> Configurations
+        {
+            get 
+            { 
+                yield return new OrderSagaMap();
+                yield return new OrderPaymentSagaMap();
+            }
+        }
+    }
+}
