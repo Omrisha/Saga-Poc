c7e46386a51e457ccbfe9c15eaec1434efd9f39d
Omri Shapira <omri.shapira@projectverte.com>
2022-07-28 14:24:54 +0300

    remove order state from payments service

--- SagaPoc.WebApplication/Program.cs
+++ SagaPoc.WebApplication/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 using SagaPoc.Messages;
 using SagaPoc.PersistanceModel;
 using SagaPoc.Sagas;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 var connString = builder.Configuration["ConnectionString"];
@@ -20,13 +22,6 @@ builder.Services.AddDbContext<OrderSagaDbContext>(options =>
 
 builder.Services.AddMassTransit(x =>
 {
-    x.AddSaga<OrderSaga>()
-    .EntityFrameworkRepository(r =>
-    {
-        r.ExistingDbContext<OrderSagaDbContext>();
-        r.ConcurrencyMode = ConcurrencyMode.Pessimistic;
-    });
-
     x.AddSaga<OrderPaymentSaga>()
     .EntityFrameworkRepository(r =>
     {
@@ -46,6 +41,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((host, log) =>
+{
+    if (host.HostingEnvironment.IsProduction())
+        log.MinimumLevel.Information();
+    else
+        log.MinimumLevel.Debug();
+
+    log.MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
+    log.MinimumLevel.Override("Quartz", LogEventLevel.Information);
+    log.WriteTo.Console();
+});
 
 var app = builder.Build();
 
